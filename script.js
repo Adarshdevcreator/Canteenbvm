0d42544cfa9ad7aa47ce2b0587701ef3513c15bc
Adarshdevcreator <71445462+Adarshdevcreator@users.noreply.github.com>
2022-11-17 22:38:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var email= document.getElementById("email").value;
+    var password= document.getElementById("password").value;
+    console.log(email);
+    console.log(password);
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+
+    // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+             var email = user.email;
+             console.log(email);
+             window.open("logged.html")
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+    });
+}
